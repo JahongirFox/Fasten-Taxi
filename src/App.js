6ea68ffa7bacf853e7072aecc3e8eef4fefec1f4
MR. ROBOT <raxmatjonovjahongir5@gmail.com>
2024-12-23 18:28:50 +0500

    Fasten1-Page-Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FastenPage1 from './Containers/FastenPage1/Fasten1.jsx'
 function App() {
   return (
     <div className="App">
-
+        <FastenPage1/>
     </div>
   );
 }
---
+++ src/Containers/FastenPage1/Fasten1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Fasten1.css'
+import FastenIMG from './fasten-img.svg'
+import FastenIMG1 from './fasten-backround-img.svg'
+function Fasten1() {
+  return (
+    <div className="Fasten1">
+        <div className="flex-Fasten1">
+            <a href={"http://localhost:3000/"}><img width={'70px'} src={FastenIMG} alt="" /></a>
+            <button className="Fasten-BTN">Скачать</button>
+        </div>
+        <div className="Fasten1-IMG">
+            <img src={FastenIMG1} alt="" />
+        </div>
+    </div>
+  );
+}
+
+export default Fasten1;
